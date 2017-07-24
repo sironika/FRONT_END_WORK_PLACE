df01b164d704b60944e9a44c6bb52a34bde9d05d
Sergiy Kolisnichenko <s.a.kolisnichenko@gmail.com>
2017-07-25 00:47:36 +0300

    -помилки+перевірки

--- home_task_5_js/progressions.js
+++ home_task_5_js/progressions.js
@@ -1,36 +1,65 @@
 
-var arPro = confirm('Ви бажаєте вибрати арифметичну прогресію?');
+var type = prompt('Введіть тип прогресії: \n a - арифметична \n g - геометрична', 'a або g');
 
-if (!arPro)
-    var geoPro = confirm('Ви бажаєте вибрати геометричну прогресію?');
-
-var a = prompt('Введіть перше число прогресії');
-// об'єднав d(різниця) та q(дільник) в b(крок)
-var b = prompt('Введіть крок прогресії');
-var n = prompt('Введіть кількість ітерацій');
-
-var text = arPro ? 'арифметичної ': geoPro ? 'геометричної':'';
-
-var result = arPro ? sumAr(a, b, n): geoPro ? sumGeo(a, b, n): 0;
-
-alert('Сума перших '+ n + ' елементів ' + text + 'прогресії становить ' + result );
-
-function sumAr(a, b, n) {
-    var number = +a;
-    var result = 0;
-    for (var i = 0; i < +n; i++) {
-        result += number;
-        number += +b;
-    }
-    return result;
+switch (type) {
+    case 'a':
+        var a1 = parseInt(prompt('Введіть перше число прогресії'));
+        while (a1 === ' ' || isNaN(a1)) {
+            alert("Введене не коректне число");
+            a1 = parseInt(prompt('Введіть перше число прогресії'));
+        }
+        var d = parseInt(prompt('Введіть різницю прогресії'));
+        while (d === ' ' || isNaN(d)) {
+            alert("Введене не коректне число");
+            d = parseInt(prompt('Введіть різницю прогресії'));
+        }
+        var an = parseInt(prompt('Введіть кількість ітерацій'));
+        while (an === ' ' || isNaN(an)) {
+            alert("Введене не коректне число");
+            an = parseInt(prompt('Введіть кількість ітерацій'));
+        }
+        alert('Сума перших ' + an + ' елементів Арифметичної прогресії становить ' + sumAr(a1, d, an));
+        break;
+    case 'g':
+        var g1 = parseInt(prompt('Введіть перше число прогресії'));
+        while (g1 === ' ' || isNaN(g1)) {
+            alert("Введене не коректне число");
+            g1 = parseInt(prompt('Введіть перше число прогресії'));
+        }
+        var q = parseInt(prompt('Введіть дільник прогресії'));
+        while (q === ' ' || isNaN(q)) {
+            alert("Введене не коректне число");
+            q = parseInt(prompt('Введіть дільник прогресії'));
+        }
+        var gn = parseInt(prompt('Введіть кількість ітерацій'));
+        while (gn === ' ' || isNaN(gn)) {
+            alert("Введене не коректне число");
+            gn = parseInt(prompt('Введіть кількість ітерацій'));
+        }
+        alert('Сума перших ' + gn + ' елементів Геометричної прогресії становить ' + sumGeo(g1, q, gn));
+        break;
+    default:
+        alert("Введений не коректний тип прогресії");
 }
 
-function sumGeo(a, b, n) {
-    var number = +a;
+function sumAr(a1, d, an) {
+    var number = a1;
     var result = 0;
-    for (var i = 0; i < +n; i++) {
+
+    for (var i = 0; i < an; i++) {
         result += number;
-        number *= +b;
+        number += d;
     }
     return result;
-}
+   }
+
+function sumGeo(g1, q, gn) {
+    var number = g1;
+    var result = 0;
+
+    for (var i = 0; i < gn; i++) {
+        result += number;
+        number *= q;
+    }
+    return result;
+    }
