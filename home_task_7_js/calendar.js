71fd6b56f741e84e38ec5b664b585fdb662b8c7d
Sergiy Kolisnichenko <s.a.kolisnichenko@gmail.com>
2017-08-14 23:21:22 +0300

    виправив недоліки

--- home_task_7_js/calendar.js
+++ home_task_7_js/calendar.js
@@ -6,33 +6,43 @@ console.log(str);
 
 var arr = str.split(",");
 
-/*for (var i = 0; i < arr.length; i++){
-console.log(arr[i]);
-}*/
+var year = {};//створив об'єкт рік
 
+// пройшовся по масиву місяців, визначивши номер і к-сть днів
+for (var i = 0; i < arr.length; i++) {
+    switch (true){
+        case (i < 7):
+            year[arr[i]] = {number: i+1, days: 31-(i%2)};
+            break;
+        case (i > 6):
+            year[arr[i]] = {number: i+1, days: 30+(i%2)};
+            break;
+    }
+}
+year['February']['days'] = 28; //виключення для лютого
+// створив об'єкти пір року
 var winter = {};
 var spring = {};
 var summer = {};
 var autumn = {};
-
-var daysCount = [31,28,31,30,31,30,31,31,30,31,30,31];
-
-winter[arr[11]] = {"number": arr.indexOf(arr[11])+1, "days": daysCount[11]};
-winter[arr[0]] = {"number": arr.indexOf(arr[0])+1, "days": daysCount[0]};
-winter[arr[1]] = {"number": arr.indexOf(arr[1])+1, "days": daysCount[1]};
-
-spring[arr[2]] = {"number": arr.indexOf(arr[2])+1, "days": daysCount[2]};
-spring[arr[3]] = {"number": arr.indexOf(arr[3])+1, "days": daysCount[3]};
-spring[arr[4]] = {"number": arr.indexOf(arr[4])+1, "days": daysCount[4]};
-
-summer[arr[5]] = {"number": arr.indexOf(arr[5])+1, "days": daysCount[5]};
-summer[arr[6]] = {"number": arr.indexOf(arr[6])+1, "days": daysCount[6]};
-summer[arr[7]] = {"number": arr.indexOf(arr[7])+1, "days": daysCount[7]};
-
-autumn[arr[8]] = {"number": arr.indexOf(arr[8])+1, "days": daysCount[8]};
-autumn[arr[9]] = {"number": arr.indexOf(arr[9])+1, "days": daysCount[9]};
-autumn[arr[10]] = {"number": arr.indexOf(arr[10])+1, "days": daysCount[10]};
-
+//розбив рік на пори року згідно номеру місяця
+for (var month in year){
+    switch (true){
+        case (year[month]['number'] < 3 || year[month]['number'] === 12):
+            winter[month] = year[month];
+            break;
+        case (year[month]['number'] < 6):
+            spring[month] = year[month];
+            break;
+        case (year[month]['number'] < 9):
+            summer[month] = year[month];
+            break;
+        case (year[month]['number'] < 12):
+            autumn[month] = year[month];
+            break;
+    }
+}
+//результат в консоль
 console.log(winter);
 console.log(spring);
 console.log(summer);
